7f0b07e4ed2637ae9294e1d9080c799f33827c81
Winnie Liang <wliang6@buffalo.edu>
2015-09-21 23:19:25 -0400

    Initial Commit

---
+++ AlphaTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlphaTracker
+//
+//  Created by Winnie Liang on 9/21/15.
+//  Copyright © 2015 Winnie Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlphaTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlphaTracker
+//
+//  Created by Winnie Liang on 9/21/15.
+//  Copyright © 2015 Winnie Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
