fce5d323a61760f47ae462a8d7eaa74ee320a1a3
Winnie Liang <wliang6@buffalo.edu>
2015-11-11 17:55:10 -0500

    Menu nav button and reset password interface included

--- FAIRWELL/FAIRWELL/ResetPasswordViewController.swift
+++ FAIRWELL/FAIRWELL/ResetPasswordViewController.swift
@@ -12,6 +12,8 @@ class ResetPasswordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
